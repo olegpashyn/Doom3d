24f4e451857d91ae6e00eafa7d7a9988345f2dcf
Vahid Kaykhaei <vkey@tobii.com>
2019-09-18 10:33:22 +0200

    Created the empty classes

---
+++ Doom3d/Doom3d/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Doom3d
+{
+    public interface ICommand
+    {
+    }
+
+    public class GameObject
+    {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+    }
+
+    public class Program
+    {
+        private readonly IList<ICommand> _userCommands = new List<ICommand>();
+
+        public static void Main()
+        {
+            var inputThread = new Thread(InputManagerLoop) { IsBackground = false };
+            var mainLoopThread = new Thread(MainGameLoop) { IsBackground = false };
+            inputThread.Start();
+            mainLoopThread.Start();
+        }
+
+        public static void MainGameLoop()
+        {
+            while (true)
+            {
+                Render();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void Render()
+        {
+        }
+
+        public static void InputManagerLoop()
+        {
+            while (true)
+            {
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        break;
+
+                    case ConsoleKey.Spacebar:
+                        break;
+
+                    case ConsoleKey.Escape:
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
